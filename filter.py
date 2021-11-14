5c56a2c9361df217f8ec8a5b14a6b030c14d2c29
SeriousDim <dlykov2001@yandex.ru>
2021-11-14 20:33:48 +0500

    3 этап

--- filter.py
+++ filter.py
@@ -8,38 +8,26 @@ def read(filename):
 
 
 def get_mean(array, px_per_brick, row_cell, col_cell):
-    mean = 0
-    for row in range(row_cell, row_cell + px_per_brick):
-        for col in range(col_cell, col_cell + px_per_brick):
-            clr_r = array[row][col][0]
-            clr_g = array[row][col][1]
-            clr_b = array[row][col][2]
-            M = (clr_r // 3 + clr_g // 3 + clr_b // 3)
-            mean += M
-    return int(mean // (px_per_brick ** 2))
+    slice = array[row_cell:row_cell + px_per_brick, col_cell:col_cell + px_per_brick]
+    return np.mean(slice)
 
 
 def update_array(array, row_cell, col_cell, px_per_brick, px_in_grade):
     mean = get_mean(array, px_per_brick, row_cell, col_cell)
-    for row in range(row_cell, row_cell + px_per_brick):
-        for col in range(col_cell, col_cell + px_per_brick):
-            array[row][col][0] = int(mean // px_in_grade) * px_in_grade
-            array[row][col][1] = int(mean // px_in_grade) * px_in_grade
-            array[row][col][2] = int(mean // px_in_grade) * px_in_grade
+    new_val = int(mean // px_in_grade) * px_in_grade
+    new_arr = np.array(3 * [new_val])
+    array[row_cell:row_cell + px_per_brick, col_cell:col_cell + px_per_brick] = new_arr
 
 
 def to_grayscale(array, px_per_brick, grades):
     rows = len(array)
     cols = len(array[1])
     px_in_grade = ceil(255 / grades)
-    row_cell = 0
-    
-    while row_cell <= rows - px_per_brick:
-        col_cell = 0
-        while col_cell <= cols - px_per_brick:
+
+    for row_cell in range(0, rows, px_per_brick):
+        for col_cell in range(0, cols, px_per_brick):
             update_array(array, row_cell, col_cell, px_per_brick, px_in_grade)
-            col_cell = col_cell + px_per_brick
-        row_cell = row_cell + px_per_brick
+
     return array
 
 
@@ -50,6 +38,6 @@ def write(array, filename):
 
 if __name__ == "__main__":
     arr = read("img2.jpg")
-    arr = to_grayscale(arr, 20, 6)
+    arr = to_grayscale(arr, 10, 6)
     write(arr, "res.jpg")
 
