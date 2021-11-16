9d365c8046dc30eee32a4d3d9ed571d49f63b2c2
Александр Васильев <76480196+c00b3r@users.noreply.github.com>
2021-11-16 14:20:24 +0500

    step #4

--- filter.py
+++ filter.py
@@ -1,12 +1,8 @@
 from PIL import Image
-import numpy as np
-img = Image.open("img2.jpg")
-arr = np.array(img)
-
 
 def find_step(n):
     return int(255 / (n - 1) - 1 if 255 % (n - 1) == 0 else 255 / (n - 1))
-#Преобразование в матричный вид
+
 
 def find_av_brightness(i, j, arr, m_h, m_w, step):
     br = 0.
@@ -14,19 +10,29 @@ def find_av_brightness(i, j, arr, m_h, m_w, step):
     br //= m_h * m_w
     br -= br % step
     return br
-#Преобразование в матричный вид
+
 
 def do_mosaic(arr, m_h, m_w, step):
     for i in range(0, len(arr), m_h):
-        for j in range(0, len(arr[1]), m_w):
-            brightness = find_av_brightness(i, j, arr, m_h, m_w, step)
+	@@ -23,10 +21,22 @@ def do_mosaic(arr, m_h, m_w, step):
             arr[i: i + m_h, j: j + m_w, :] = brightness
 
 
+inp_im_names = input('Введите название изображение '
+                     'Ввод выглядит как (например: img2,res): ') \
+    .split(',')
+
 
 inp_sizes = input('Введите ширину и высоту мозайки (например: 100,100): ')
+
+
 num_grad = int(input('Enter the number of gradations(for example: 6): '))
+
+
 e_h, e_w = map(int, inp_sizes.split(','))
-do_mosaic(inp_im, e_h, e_w, find_step(num_grad)) 
-res = Image.fromarray(arr)
-res.save('res.jpg')
+do_mosaic(inp_im, e_h, e_w, find_step(num_grad))
+
+res = Image.fromarray(inp_im)
+
+
+res.save(inp_im_names[1] + ".jpg")
