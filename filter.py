4fd970164285ddfec8733b8e5192b0d0acd85176
Roman <roman.teckk@gmail.com>
2021-11-17 15:21:12 +0500

    2 Этап - разбил на функции, вынес размер мозаики, градации в параметры преобразования + форматирование

--- filter.py
+++ filter.py
@@ -1,32 +1,47 @@
 from PIL import Image
 import numpy as np
 import easygui
+from collections import namedtuple
+
+Size = namedtuple("Size", ["width", "height"])
+
+def open_image_as_array(path = ""):
+    if not path:
+        path = easygui.fileopenbox(title="Открыть изображение", filetypes=["*.jpg"])
+    raw_img = Image.open(path)
+    return np.array(raw_img)
+
+def save_image(image, path = ""):
+    if not path:
+        path = easygui.filesavebox(title="Сохранить изображение", filetypes=["*.jpg"])
+    image.save(path)
+
+def get_mosaic(raw_image, mosaic_size = Size(10, 10), discrete_step = 50):
+    
+    width = len(raw_image)
+    height = len(raw_image[1])
+
+    for i in range(0, width - mosaic_size.width + 1, mosaic_size.width):
+        for j in range(0, height - mosaic_size.height + 1, mosaic_size.height):
+            average_brightness = 0
+            for x in range(i, i + mosaic_size.width):
+                for y in range(j, j + mosaic_size.height):
+                    r = raw_image[x][y][0]
+                    g = raw_image[x][y][1]
+                    b = raw_image[x][y][2]
+                    pixel_brightness = (r // 3 + g // 3 + b // 3)
+                    average_brightness += pixel_brightness 
+            average_brightness = int(average_brightness // (mosaic_size.width * mosaic_size.height))
+            for x in range(i, i + mosaic_size.width):
+                for y in range(j, j + mosaic_size.height):
+                    raw_image[x][y][0] = int(average_brightness // discrete_step) * discrete_step
+                    raw_image[x][y][1] = int(average_brightness // discrete_step) * discrete_step
+                    raw_image[x][y][2] = int(average_brightness // discrete_step) * discrete_step
+
+    return Image.fromarray(raw_image)
 
 
-img = Image.open(easygui.fileopenbox(title="Открыть изображение", filetypes=["*.jpg"]))
-pixels = np.array(img)
 
-width = len(pixels)
-height = len(pixels[1])
-i = 0
-while i <= width - 10:
-    j = 0
-    while j <= height - 10:
-        sum = 0
-        for x in range(i, i + 10):
-            for y in range(j, j + 10):
-                r = pixels[x][y][0]
-                g = pixels[x][y][1]
-                b = pixels[x][y][2]
-                M = (r // 3 + g // 3 + b // 3)
-                sum += M
-        sum = int(sum // 100)
-        for x in range(i, i + 10):
-            for y in range(j, j + 10):
-                pixels[x][y][0] = int(sum // 50) * 50
-                pixels[x][y][1] = int(sum // 50) * 50
-                pixels[x][y][2] = int(sum // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(pixels)
-res.save(easygui.filesavebox(title="Сохранить изображение", filetypes=["*.jpg"]))
+raw_image = open_image_as_array()
+mosaic = get_mosaic(raw_image)
+save_image(mosaic)
