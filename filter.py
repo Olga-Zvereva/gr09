ad4f2abbd6ee565f64b207390593d1a677006d78
Roman <roman.teckk@gmail.com>
2021-11-17 15:50:39 +0500

    3 этап - матричные преобразования

--- filter.py
+++ filter.py
@@ -5,41 +5,44 @@ from collections import namedtuple
 
 Size = namedtuple("Size", ["width", "height"])
 
-def open_image_as_array(path = ""):
+
+def open_image_as_array(path=""):
     if not path:
-        path = easygui.fileopenbox(title="Открыть изображение", filetypes=["*.jpg"])
+        path = easygui.fileopenbox(
+            title="Открыть изображение", filetypes=["*.jpg"])
     raw_img = Image.open(path)
     return np.array(raw_img)
 
-def save_image(image, path = ""):
+
+def save_image(image, path=""):
     if not path:
-        path = easygui.filesavebox(title="Сохранить изображение", filetypes=["*.jpg"])
+        path = easygui.filesavebox(
+            title="Сохранить изображение", filetypes=["*.jpg"])
     image.save(path)
 
-def get_mosaic(raw_image, mosaic_size = Size(10, 10), discrete_step = 50):
-    
+
+def get_mosaic(raw_image, mosaic_size=Size(10, 10), discrete_step=50):
+
     width = len(raw_image)
     height = len(raw_image[1])
 
     for i in range(0, width - mosaic_size.width + 1, mosaic_size.width):
         for j in range(0, height - mosaic_size.height + 1, mosaic_size.height):
             average_brightness = 0
-            for x in range(i, i + mosaic_size.width):
-                for y in range(j, j + mosaic_size.height):
-                    r = raw_image[x][y][0]
-                    g = raw_image[x][y][1]
-                    b = raw_image[x][y][2]
-                    pixel_brightness = (r // 3 + g // 3 + b // 3)
-                    average_brightness += pixel_brightness 
-            average_brightness = int(average_brightness // (mosaic_size.width * mosaic_size.height))
-            for x in range(i, i + mosaic_size.width):
-                for y in range(j, j + mosaic_size.height):
-                    raw_image[x][y][0] = int(average_brightness // discrete_step) * discrete_step
-                    raw_image[x][y][1] = int(average_brightness // discrete_step) * discrete_step
-                    raw_image[x][y][2] = int(average_brightness // discrete_step) * discrete_step
+            average_brightness = np.sum(
+                raw_image[i: i + mosaic_size.width, j: j + mosaic_size.height]) / 3 // (mosaic_size.width * mosaic_size.height)
 
+            # for x in range(i, i + mosaic_size.width):
+            #     for y in range(j, j + mosaic_size.height):
+            #         raw_image[x][y][0] = int(
+            #             average_brightness // discrete_step) * discrete_step
+            #         raw_image[x][y][1] = int(
+            #             average_brightness // discrete_step) * discrete_step
+            #         raw_image[x][y][2] = int(
+            #             average_brightness // discrete_step) * discrete_step
+            raw_image[i: i + mosaic_size.width, j: j + mosaic_size.height:] = int(
+                average_brightness // discrete_step) * discrete_step
     return Image.fromarray(raw_image)
-
 
 
 raw_image = open_image_as_array()
