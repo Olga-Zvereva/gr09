a26e2fed32a0ed4d147865e8daf2032aa6839b1e
DESKTOP-JIA02LP\Raph <abdyev_r@mail.ru>
2021-11-17 17:22:39 +0500

    step 2

--- filter.py
+++ filter.py
@@ -1,27 +1,38 @@
 from PIL import Image
 import numpy as np
+
+
+def filter_image(array, mosaic_size):
+    a = len(array) - mosaic_size + 1
+    a1 = len(array[1]) - mosaic_size + 1
+    for i in range(0, a, mosaic_size):
+        for j in range(0, a1, mosaic_size):
+            average_brightness = get_brightness(array, mosaic_size, i, j)
+            array = paint_over_pixels(array, average_brightness, step, mosaic_size, i, j)
+    return array
+
+
+def get_brightness(array, mosaic_size, i, j):
+    sum = 0
+    for x in range(i, i + mosaic_size):
+        for y in range(j, j + mosaic_size):
+            for num in range(3):
+                sum += int(array[x][y][num])
+    brightness = int(sum // (3 * mosaic_size ** 2))
+    return brightness
+
+
+def paint_over_pixels(array, brightness, step, mosaic_size, i, j):
+    for x in range(i, i + mosaic_size):
+        for y in range(j, j + mosaic_size):
+            for num in range(3):
+                array[x][y][num] = int(brightness // step) * step
+    return array
+
+
 img = Image.open("img2.jpg")
-arr = np.array(img)
-a = len(arr)
-a1 = len(arr[1])
-i = 0
-while i < a - 10:
-    j = 0
-    while j < a1 - 10:
-        s = 0
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                r = int(arr[n][n1][0])
-                g = int(arr[n][n1][1])
-                b = int(arr[n][n1][2])
-                s += (r + g + b) / 3
-        s = int(s // 100)
-        for n in range(i, i + 10):
-            for n1 in range(j, j + 10):
-                arr[n][n1][0] = int(s // 50) * 50
-                arr[n][n1][1] = int(s // 50) * 50
-                arr[n][n1][2] = int(s // 50) * 50
-        j = j + 10
-    i = i + 10
-res = Image.fromarray(arr)
+array = np.array(img)
+mosaic_size = 10
+step = 50
+res = Image.fromarray(filter_image(array, mosaic_size))
 res.save('res.jpg')
