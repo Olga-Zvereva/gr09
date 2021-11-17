a1c453e9a77535df8b7d29fb4cfdaf2f23c6b208
dimasikBurdin <81648493+dimasikBurdin@users.noreply.github.com>
2021-11-17 14:22:11 +0500

    3,4

--- filter.py
+++ filter.py
@@ -15,25 +15,30 @@ def createFilter(size, step):
 
 def getBrightness(x, y, size):
     res = 0
-    for x in range(x, x + size):
-        for y in range(y, y + size):
-            for e in imageToArray[x][y]:
+    for x1 in range(x, x + size):
+        for y1 in range(y, y + size):
+            for e in imageToArray[x1][y1]:
                 res += e // 3
     return res // size ** 2
 
 
 def getColorPix(brightness, x, y, size, grayscale):
-    for x in range(x, x + size):
-        for y in range(y, y + size):
-            for e in imageToArray[x][y]:
+    for x1 in range(x, x + size):
+        for y1 in range(y, y + size):
+            for e in imageToArray[x1][y1]:
                 e = brightness // grayscale * grayscale
 
 
+print('Введите имя исходного изображения:')
+nameImage = input()
+print('Введите имя результирующего изображения:')
+resImage = input()
+
 size = int(input())
 step = int(input())
-imageToArray = np.array(Image.open("img2.jpg"))
+imageToArray = np.array(Image.open(nameImage))
 
 createFilter(size, step)
 
 resultImage = Image.fromarray(imageToArray)
-resultImage.save('res.jpg') 
+resultImage.save(resImage) 
