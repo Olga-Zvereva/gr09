e9f0057e041e90f86a61893dae5f4c02f6ffc5d1
Churkin-Kirill <kirill.churkin.02@mail.ru>
2021-11-24 22:59:34 +0500

    Профилирование

---
+++ filter_with_filename.py
@@ -0,0 +1,35 @@
+from PIL import Image
+import numpy as np
+
+
+def get_average_brightness(array_of_pixels, mosaic_size, i, j):
+    area = array_of_pixels[i:i + mosaic_size, j:j + mosaic_size]
+    total_brightness = np.sum(area)
+    return int(total_brightness // 3 // mosaic_size ** 2)
+
+
+def change_pixels_color(array_of_pixels, mosaic_size, grayscale, average_brightness, i, j):
+    pixel_value = int(average_brightness // grayscale) * grayscale
+    array_of_pixels[i:i + mosaic_size, j:j + mosaic_size] = pixel_value
+    return array_of_pixels
+
+
+def grey_filter(image, mosaic_size, grayscale):
+    array_of_pixels = np.array(image)
+    height = len(array_of_pixels)
+    width = len(array_of_pixels[1])
+    for i in range(0, height - mosaic_size + 1, mosaic_size):
+        for j in range(0, width - mosaic_size + 1, mosaic_size):
+            average_brightness = get_average_brightness(array_of_pixels, mosaic_size, i, j)
+            array_of_pixels = change_pixels_color(array_of_pixels, mosaic_size, grayscale, average_brightness, i, j)
+    return array_of_pixels
+
+
+if __name__ == "__main__":
+    name_of_input_image = 'img2.jpg'
+    mosaic_size = 10
+    grayscale = 50
+    name_of_output_image = 'res.jpg'
+    input_image = Image.open(name_of_input_image)
+    image_in_gray_tones = Image.fromarray(grey_filter(input_image, mosaic_size, grayscale))
+    image_in_gray_tones.save(name_of_output_image)
